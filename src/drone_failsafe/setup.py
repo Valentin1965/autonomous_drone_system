db8fe30835259b8a291ef9d29557a28c3750b187
Valentin1965 <land.market0@gmail.com>
2026-05-24 09:59:06 +0300

    Add RTL and failsafe module

---
+++ src/drone_failsafe/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'drone_failsafe'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Valentyn',
+    maintainer_email='you@example.com',
+    description='Failsafe module for autonomous drone',
+    entry_points={
+        'console_scripts': [
+            'drone_failsafe = drone_failsafe.failsafe_node:main',
+        ],
+    },
+)
